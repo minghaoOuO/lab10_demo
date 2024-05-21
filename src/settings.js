3e25c5265487dfa0311062cb6ef5273e778d9066
minghaoOuO <ming.hao.hsieh0914@gmail.com>
2024-05-21 13:38:32 +0800

    initial commit

---
+++ src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default","platform"],collisionMatrix:[[true,true],[true,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/game.fire",orientation:"",jsList:[]};
